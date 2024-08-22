6f7b5b50065c1c3ea6ac9189edc17e7801ae0d5d
TRISTAN ERZ“git config --global user.email „erztristan@berkeley.edu <your_name@berkeley.edu>
2024-08-22 13:22:08 -0700

    Doc-Strings

--- newton.py
+++ newton.py
@@ -1,8 +1,21 @@
 def f(x):
+    """Get back the value of a function to a defined point.
+
+    Keyword arguments:
+    x -- the parameter
+    """
     return (x + 3) * (x + 3)
 
 
 def optimize(xt, xt1, epsilon, accuracy):
+    """Get back the zero point for a function defined in f(x)
+
+    Keyword arguments:
+    xt -- start of the optimization
+    xt1 -- second start point of the optimization
+    epsilon -- value for the calculation of the derivation
+    accuracy -- value when the algorithmus stop
+    """
     while abs(xt - xt1) > accuracy:
         f1 = (f(xt1 + epsilon) - f(xt1)) / epsilon
         f1epsilon = (f(xt1 + epsilon + epsilon) - f(xt1 + epsilon)) / epsilon
