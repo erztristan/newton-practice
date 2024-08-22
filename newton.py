a1c721dea85cefa80f768bf5134e48abf8978e44
TRISTAN ERZ“git config --global user.email „erztristan@berkeley.edu <your_name@berkeley.edu>
2024-08-22 13:17:14 -0700

    New

--- newton.py
+++ newton.py
@@ -1,18 +1,19 @@
 def f(x):
-    return (x+3)*(x+3)
+    return (x + 3) * (x + 3)
 
-def optimize (xt, xt1, epsilon, accuracy):
-    
-    while abs(xt-xt1) > accuracy:
-        f1 = (f(xt1+epsilon) - f(xt1)) / epsilon
-        f1epsilon = (f(xt1+epsilon+epsilon) - f(xt1+epsilon)) / epsilon
-        f2 =  (f1epsilon - f1) / epsilon
-    
+
+def optimize(xt, xt1, epsilon, accuracy):
+    while abs(xt - xt1) > accuracy:
+        f1 = (f(xt1 + epsilon) - f(xt1)) / epsilon
+        f1epsilon = (f(xt1 + epsilon + epsilon) - f(xt1 + epsilon)) / epsilon
+        f2 = (f1epsilon - f1) / epsilon
+
         a = xt
-        
+
         xt = xt1 - f1 / f2
         xt1 = a
-    
+
     return xt
 
-optimize(100, 1, 0.0001, 0.0001)
+
+print(optimize(100, 1, 0.0001, 0.0001))
