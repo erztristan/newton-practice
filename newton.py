da8781c1497a5cb5d3b89a619df9e5f09b957f79
TRISTAN ERZ“git config --global user.email „erztristan@berkeley.edu <your_name@berkeley.edu>
2024-08-23 08:40:59 -0700

    Add newest version

--- newton.py
+++ newton.py
@@ -7,26 +7,22 @@ def f(x):
     return (x + 3) * (x + 3)
 
 
-def optimize(xt, xt1, epsilon, accuracy):
-    """Get back the zero point for a function defined in f(x)
+def optimize (f, xt, xt1, epsilon, accuracy):
 
-    Keyword arguments:
-    xt -- start of the optimization
-    xt1 -- second start point of the optimization
-    epsilon -- value for the calculation of the derivation
-    accuracy -- value when the algorithmus stop
-    """
-    while abs(xt - xt1) > accuracy:
-        f1 = (f(xt1 + epsilon) - f(xt1)) / epsilon
-        f1epsilon = (f(xt1 + epsilon + epsilon) - f(xt1 + epsilon)) / epsilon
-        f2 = (f1epsilon - f1) / epsilon
+    if type(xt) != int:
+        raise TypeError(f"xt must be numeric")
+         
+    while abs(xt-xt1) > accuracy:
+
+        f1 = (f(xt1+epsilon) - f(xt1)) / epsilon
+        f1epsilon = (f(xt1+epsilon+epsilon) - f(xt1+epsilon)) / epsilon
+        f2 =  (f1epsilon - f1) / epsilon
 
         a = xt
-
+        
         xt = xt1 - f1 / f2
         xt1 = a
-
+    
     return xt
 
 
-print(optimize(100, 1, 0.0001, 0.0001))
